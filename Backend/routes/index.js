e0b2a33d940859f89584eaed3c1fc3dacf76764a
Jean-baptiste <jeanbaptiste.griveaud@gmail.com>
2021-02-26 18:08:50 +0100

    Préparation des routes

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -6,30 +6,73 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Sign-up -> Inscription 
+  Body :
+      REQUIRED ---> email : (quentin@gmail.com), password : (XXXXXX), pseudo : (HelicoptèreDeCombat), birthDate : (12/23/1992) --> vérification majorité , problems_types : String
+      OPTIONALE --> problem_description : String, localisation : String, gender : String, avatar : String, 
+  Response : result (true), token (1234), birthDate : (12/23/1992), problems_types : String, localisation : String
+*/
 router.post('/sign-up', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* sign-in -> Connexion
+body : email : (quentin@gmail.com), password : (XXXXXX)
+response : result (true), token : 1234
+*/
 router.post('/sign-in', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* sign-out -> déconnexion
+body : token : 1234, 
+response : result (true), 
+*/
 router.post('/sign-out', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* show-card -> afficher les autres utilisateurs dans le swipe.
+query : token : 1234, birthDate : (12/23/1992), problems_types : String, localisation : String, gender : String, 
+response : userFiltered : array, pseudo (celui du user connecté) : String
+*/
 router.get('/show-card', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* show-msg -> afficher les différentes conversations avec les users.
+query : token 1234
+response : [{pseudo (sender) : String, date (dernier message) : date, avatar (sender) : String, last_message : String, demande : boolean, is_read (nombre de messages non-lus) : Number, delete : boolean, conversation_id : 1234}]
+*/
 router.get('/show-msg', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* show-convers -> afficher la conversation avec les autres utilisateurs.
+query : conversation_id : 1234
+response : collection message qui est liée et conversation_id.
+*/
 router.get('/show-convers', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* first-message -> écriture du premier message avec création de la convers en base.
+body :
+response : 
+*/
+router.post('/first-msg', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* send-msg -> envoyer un message.
+body : conversation_id : 1234
+response : 
+*/
 router.post('/send-msg', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
