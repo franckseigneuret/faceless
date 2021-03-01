7bbe687a8b79f46bb0cb4b3701c84ece24820d56
Jean-baptiste <jeanbaptiste.griveaud@gmail.com>
2021-03-01 14:39:52 +0100

    nouveau navbar

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,38 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 
-
-//COMPONENTS
-import Quizz from './components/Quizz'
-import OptionalQuizz from './components/OptionalQuizz'
-import Registration from './components/Registration'
-
-// NAVIGATION
-import { NavigationContainer } from '@react-navigation/native';
+import {Image,StyleSheet} from 'react-native';
+import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MessageScreen from './screens/MessageScreen';
+import HomeScreen from './screens/HomeScreen';
+import ProfilScreen from './screens/ProfilScreen';
+
+import { Ionicons } from '@expo/vector-icons'; 
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
+const BottomNavigator = () => {
+    return (
+        <Tab.Navigator
+        screenOptions={({ route }) => ({
+            tabBarLabel:() => {return null},
+            tabBarIcon: ({ color, focused }) => {
+            let iconName;
 
-
-export default function App() {
-  return (
-    <NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Registration" component={Registration} />
-      <Stack.Screen name="Quizz" component={Quizz} />
-      <Stack.Screen name="OptionalQuizz" component={OptionalQuizz} />
-    </Stack.Navigator>
-  </NavigationContainer>
-  );
+            if (route.name == 'HomeScreen') {
+                if(focused){
+                    return <Image
+                style={{ width: 60, height: 50,}}
+                source={{
+                    uri:'https://i.imgur.com/ZZ1OGjy.png',
+                }}
+            />
+                  }else {
+                    return <Image
+                    style={{ width: 60, height: 50,}}
+                    source={{
+                        uri:'https://i.imgur.com/ISNBhdh.png',
+                    }}
+                />
+                  }
+            } else if (route.name == 'MessageScreen') {
+                iconName = 'chatbubbles';
+                return <Ionicons name={iconName} size={30} color={color} />;
+            } else if (route.name == 'ProfilScreen') {
+                iconName = 'person-circle-sharp';
+                return <Ionicons name={iconName} size={30} color={color} />;
+            }
+            },
+        })}
+            
+        tabBarOptions={{
+            activeTintColor: '#5571D7',
+            inactiveTintColor: '#BCC8F0',
+            style: {
+                backgroundColor: '#FFF6EF',
+                height: 60,
+            }
+        }}
+        
+        >
+            
+            <Tab.Screen name="ProfilScreen" component={ProfilScreen} />
+            <Tab.Screen name="HomeScreen" component={HomeScreen} />
+            <Tab.Screen name="MessageScreen" component={MessageScreen} />
+            
+        </Tab.Navigator>
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF1E2',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+            <Stack.Screen name="BottomNavigator" component={BottomNavigator} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
