d1d345c67e071c0254dd23d474e1889b109c5f5e
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-03-01 15:43:59 +0100

    géolocalisation: fetch sur API + sélection ville parmi les villes suggérées

--- Frontend/App.js
+++ Frontend/App.js
@@ -7,7 +7,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Quizz from './components/Quizz'
 import OptionalQuizz from './components/OptionalQuizz'
 import Registration from './components/Registration'
-
+import Geolocalisation from './components/Geolocalisation'
 // NAVIGATION
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -19,12 +19,13 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Registration" component={Registration} />
-      <Stack.Screen name="Quizz" component={Quizz} />
-      <Stack.Screen name="OptionalQuizz" component={OptionalQuizz} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Geolocalisation" component={Geolocalisation} />
+        <Stack.Screen name="Registration" component={Registration} />
+        <Stack.Screen name="Quizz" component={Quizz} />
+        <Stack.Screen name="OptionalQuizz" component={OptionalQuizz} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- Frontend/components/Geolocalisation.js
+++ Frontend/components/Geolocalisation.js
@@ -1,12 +1,80 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 
 export default function Geolocalisation(props) {
 
+  const [townList, setTownList] = useState([])
+  const [search, setSearch] = useState('')
+  const [selectedTown, setSelectedTown] = useState(null)
+
+  const onChangeText = async (search) => {
+    setSearch(search)
+    // console.log('-------')
+    // console.log(search)
+    // console.log('-------')
+
+    // fetcher les villes dès qu'il y a plus de 2 caractères saisis dans le champs
+    if (search.length > 2) {
+
+      const uri = `https://api-adresse.data.gouv.fr/search/?q=${search}&type=municipality&autocomplete=1`
+      const data = await fetch(uri)
+      const body = await data.json()
+      const townsAPI = body.features
+      const townsApiName = []
+      townsAPI && townsAPI.map((town) => {
+        townsApiName.push(town.properties.label)
+      })
+
+      setTownList(townsApiName)
+    }
+  }
+
+  const TownListComponent = townList.map((item, i) => {
+    const styleItem = [styles.town]
+    selectedTown === item ? styleItem.push(styles.townSelected) : ''
+    return (
+      <Text
+        key={i}
+        style={styleItem}
+        className="town-item"
+        onPress={() => { setSelectedTown(item) }}
+      >{item}</Text>
+    )
+  }
+  )
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Geolocalisation :) </Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: 50 }}>
+      <Text style={styles.title}>
+        Tu viens d'où ? {selectedTown}</Text>
+
+      <TextInput
+        style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={text => onChangeText(text)}
+        value={search}
+        placeholder={'Votre ville ?'}
+      />
+      {TownListComponent}
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  title: {
+    color: '#5571D7',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  town: {
+    backgroundColor: 'yellow'
+  },
+  townSelected: {
+    backgroundColor: 'blue'
+  },
+})
