6f1e623bdc4d4b841f51c852e4d3318ceb9c63ef
Franck Seigneuret <franck.seigneuret@gmail.com>
2021-03-01 11:55:46 +0100

    initialisation fichier geolocalisation

---
+++ Frontend/components/Geolocalisation.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+
+
+export default function Geolocalisation(props) {
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Geolocalisation :) </Text>
+    </View>
+  )
+}
